3cc4b00ba20cb0c97905b5f890bbbf7fcd19ea93
runerback <runerback@qq.com>
2019-01-16 18:24:08 +0800

    bug in alg

--- MinimalConvexService.cs
+++ MinimalConvexService.cs
@@ -80,22 +80,82 @@ namespace GetLargestES
             SetConvexPathData(source, geometry);
         }
 
-        int[] CalculateMinimalConvex(PointData[] points)
+        IEnumerable<PointData> CalculateMinimalConvex(PointData[] points)
         {
             var count = points?.Length ?? 0;
             if (count < 3)
-                return null;
-
+                yield break;
+            
             //calculate
 
+            //step one: find the point with lowest y and lowest x
+            var P = points
+                .OrderBy(item => item.Point.Y)
+                .OrderBy(item => item.Point.X)
+                .First();
 
-            return points.Select(item => item.Index).ToArray();
+            var right = new Vector(1, 0);
+            var others = OrderByAngle(
+                points.Where(item => item.Index != P.Index), P.Point)
+                .ToArray();
+            
+            var stack = new Stack<PointData>();
+            stack.Push(P);
+            stack.Push(others[0]);
+            stack.Push(others[1]);
+
+            for (int i = 2, j = count - 1; i < j;)
+            {
+                var item3 = others[i];
+                var item2 = stack.Pop();
+                var item1 = stack.Peek();
+
+                var p3 = item3.Point;
+                var p2 = item2.Point;
+                var p1 = item1.Point;
+
+                var dir = (p2.X - p1.X) * (p3.Y - p1.Y) - (p2.Y - p1.Y) * (p3.X - p1.X);
+                if (dir > 0)
+                    stack.Push(item2);
+                else
+                    item2.ToggleCheckedState();
+
+                stack.Push(item3);
+                i++;
+            }
+
+            while (stack.Count > 0)
+                yield return stack.Pop();
+        }
+
+        IEnumerable<PointData> OrderByAngle(IEnumerable<PointData> source, Point basePoint)
+        {
+            foreach (var group in source
+                .GroupBy(item => item.Point.X.CompareTo(basePoint.X))
+                .OrderByDescending(item => item.Key))
+            {
+                switch (group.Key)
+                {
+                    case 0:
+                        foreach (var item in group.OrderBy(item => item.Point.Y))
+                            yield return item;
+                        break;
+                    default:
+                        foreach (var item in group.OrderBy(item =>
+                        {
+                            var p = item.Point;
+                            return (p.Y - basePoint.Y) / Math.Abs(p.X - basePoint.X);
+                        }))
+                            yield return item;
+                        break;
+                }
+            }
         }
 
         PathFigure GenerateRootFigure(PointData[] points, Func<PointData, PointPresenter> itemContainerSelector)
         {
-            var indexes = CalculateMinimalConvex(points);
-            var count = indexes?.Length ?? 0;
+            var orderedPoints = CalculateMinimalConvex(points).ToArray();
+            var count = orderedPoints?.Length ?? 0;
             if (count < 3)
                 return null;
 
@@ -108,7 +168,7 @@ namespace GetLargestES
                 {
                     Path = new PropertyPath(PositionExtension.SegmentPositionProperty),
                     Mode = BindingMode.OneWay,
-                    Source = itemContainerSelector(points[indexes[0]])
+                    Source = itemContainerSelector(orderedPoints[0])
                 });
 
             var segments = figure.Segments;
@@ -122,7 +182,7 @@ namespace GetLargestES
                     {
                         Path = new PropertyPath(PositionExtension.SegmentPositionProperty),
                         Mode = BindingMode.OneWay,
-                        Source = itemContainerSelector(points[indexes[i]])
+                        Source = itemContainerSelector(orderedPoints[i])
                     });
                 segments.Add(segment);
             }
